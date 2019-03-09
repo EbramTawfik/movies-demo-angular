7d1b38fb97fbfca853bcaa1b56a5e07f1916897b
Ebram_Tawfik <ebram.s.tawfik@gmail.com>
2019-03-08 22:43:49 -0800

    Movies Details

--- src/app/movies-details-page/movies-details-page.component.ts
+++ src/app/movies-details-page/movies-details-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MoviesHelper } from '../helpers/MoviesHelper';
+import { Movie } from '../helpers/Movie';
 
 @Component({
   selector: 'app-movies-details-page',
@@ -7,14 +9,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./movies-details-page.component.scss']
 })
 export class MoviesDetailsPageComponent implements OnInit {
-  movieId: string;
+  selectedMovie: Movie;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.movieId = params['id'];
-      console.log(params); //log the entire params object
-      console.log(params['id']); //log the value of id
+      const movieId: string = params['id'];
+      this.selectedMovie = MoviesHelper.GetMovieById(movieId);
+      console.log(params); // log the entire params object
+      console.log(params['id']); // log the value of id
     });
   }
 }
