1180d909e789302f80398d8c7b82db1c3854f97f
Ebram_Tawfik <ebram.s.tawfik@gmail.com>
2019-03-08 21:32:21 -0800

    Movies Material Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MoviesMainPageComponent } from './movies-main-page/movies-main-page.component';
 import { MoviesDetailsPageComponent } from './movies-details-page/movies-details-page.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 @NgModule({
-  declarations: [
-    AppComponent,
-    MoviesMainPageComponent,
-    MoviesDetailsPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, MoviesMainPageComponent, MoviesDetailsPageComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule, MatRippleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
